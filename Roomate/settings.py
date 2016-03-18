bea05b4d996f2ba530a45bd3494d625213310e81
asier <asiersantosval@gmail.com>
2016-03-18 12:35:51 +0100

    intento enesimo +2 de modificacion para heroku

--- Roomate/settings.py
+++ Roomate/settings.py
@@ -146,18 +146,8 @@ ALLOWED_HOSTS = ['*']
 #-----------------------------------------------------------------------------
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-    }
-}
-
-# Update database configuration with $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+DATABASES['default'] =  dj_database_url.config()
 
 
 #-----------------------------------------------------------------------------
